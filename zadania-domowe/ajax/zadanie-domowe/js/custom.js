552ebaa85f08cc767ebc3bc319845bbe49901461
Katarzyna <figarskak@gmail.com>
2018-02-28 20:50:13 +0100

    poprawka

--- zadania-domowe/ajax/zadanie-domowe/js/custom.js
+++ zadania-domowe/ajax/zadanie-domowe/js/custom.js
@@ -1,18 +1,20 @@
 'use strict';
 
 
-$('#przycisk').click(function(){
- 
-$.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data) {
-    
-    
+$('#przycisk').click(function () {
 
-    $('body').append('<p>Imię: '+data.imie+'</p>');
-    $('body').append('<p>Nazwisko: '+data.nazwisko+'</p>');
-    $('body').append('<p>Zawód: '+data.zawod+'</p>');
-    $('body').append('<p>Firma: '+data.firma+'</p>');
-   
-    
-});
+    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
+
+
+
+        $('body').append('<div id="dane-programisty"></div>');
+
+        $('#dane-programisty').append('<p>Imię: ' + data.imie + '</p>');
+        $('#dane-programisty').append('<p>Nazwisko: ' + data.nazwisko + '</p>');
+        $('#dane-programisty').append('<p>Zawód: ' + data.zawod + '</p>');
+        $('#dane-programisty').append('<p>Firma: ' + data.firma + '</p>');
+
+
+    });
 
 });
